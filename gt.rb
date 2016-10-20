683670d465632944b61204ecacdd5568f39c1ed4
root <ben.griffin@shorrkan.eu>
2016-10-20 17:37:43 +0200

    First draft for import tool, Work in progress

--- gt.rb
+++ gt.rb
@@ -6,42 +6,142 @@ require 'fileutils'
 require 'bgg'
 require 'parseconfig'
 require 'cld'
+require 'optparse'
 
 
-
-def createCollections(text, db, users)
-
-	text.each { |line| 
-		data = line.split(',')
-		game = data[1]
-		
-		res = BggApi.search(type: 'boardgame', query: game, exact: 1)
-		
-		if res['total'].to_i == 0
-			res = BggApi.search(type: 'boardgame', query: game, exact: 0)
+def createCollections(games, db)
 	
-		if res['total'].to_i > 1
-			games = res['item']
-			primaryGame = games.slice!(0)
-			
-			print "Best match found: #{primaryGame['name'][0]['value']}, Year: #{primaryGame['yearpublished'][0]['value']}. Confirm? [Y/n]"
-			answer = $stdin.gets.chomp
-			
-			if answer == 'n'
-				x = 0
-				
-				games.each { |game| 
-					puts "#{x}. Name: #{game['name'][0]['value']}, Year: #{primaryGame['yearpublished'][0]['value']}"
-					x += 1
-				}
-			
-			end
-		end
+	games.each { |item| 
+		db.query("INSERT into collections(id_user, id_boardgame, language) values(#{item['id_user']}, #{item['id_boardgame']}, '#{item['language']}');")
 	}
+	
+end
+
+def gameLookup(text, users)
+	collections = []
+	
+	text.each { |line|
+		title = {}
+		choice = 0
+		found = false 
+		data = line.split(',')
+		name = data.first
+		gameQuery = data[1]
+		quantity = data[2].to_i
+		
+		if quantity > 1
+			puts "Quantity > 1, checking for existing entry.."
+			collections.each { |item| 
+				if item['gameQuery'] == gameQuery
+					puts "Existing entry has been found"
+					found = true
+					title.merge!({ 'gameQuery' => gameQuery, 'id_boardgame' => item['id_boardgame'], 'language' => item['language']})
+					break
+				end 
+			}
+		end
+		
+		if not found
+			sleep(0.5)
+			puts "Searching for #{gameQuery}.."
+			res = BggApi.search(type: 'boardgame', query: gameQuery, exact: 1)
+			res = BggApi.search(type: 'boardgame', query: gameQuery, exact: 0) if res['total'].to_i == 0
+		
+			if res['total'].to_i == 0
+				words = gameQuery.split(' ')
+				words.slice!(-1)
+				newQuery = words.join(' ')
+				res = BggApi.search(type: 'boardgame', query: newQuery, exact: 0)			
+			end
+		
+			if res['total'].to_i > 0
+				games = res['item']
+		
+				if res['total'].to_i > 1
+					primaryGame = games.slice!(0)
+			
+					if primaryGame.has_key? 'yearpublished'
+						year = ", Year: #{primaryGame['yearpublished'].first['value']}"
+					else
+						year = ""
+					end
+			
+					print "Best match found: #{primaryGame['name'].first['value']}#{year}. Confirm? [Y/n]"
+					answer = $stdin.gets.chomp
+			
+					if answer == 'n'
+						x = 0
+				
+						games.each { |game| 
+							test_var(game)
+					
+							if game.has_key? 'yearpublished'
+								year = ", Year: #{game['yearpublished'].first['value']}"
+							else
+								year = ""
+							end
+					
+							puts "#{x}. Name: #{game['name'].first['value']}#{year}"
+							x += 1
+						}
+			
+						puts
+						answer = -1
+				
+						while answer < 0
+							print "Multiple matches Found; select the most appropriate one: "
+							answer = $stdin.gets.chomp.to_i
+						end
+				
+						choice = answer				
+					end
+				end
+			
+				gameName = games[choice]['name'].first['value']
+				language = CLD.detect_language(gameName)
+		
+				if not language[:reliable]
+					print "Language detected is #{language[:name]}. Confirm? [Y/n]"
+					answer = $stdin.gets.chomp
+			
+					if answer == 'n'
+						print "Please insert title language ISO code (ie. en,fr): "
+						language[:code] = $stdin.gets[0..1]
+					end
+				else
+					puts "Language for #{gameName} reliably detected as #{language[:name]}"
+				end
+		
+				title.merge!({ 'gameQuery' => gameQuery, 'id_boardgame' => games[choice]['id'].to_i, 'language' => language[:code]})
+			else 
+				print "No suitable match has been found (typo?). Please insert correct name to search for: "
+				newgame = $stdin.gets.chomp
+				puts "gameQuery: #{gameQuery}, line: #{line.chomp}, newgame: #{newgame}"
+				line.sub!(/,.*?,/, ",#{newgame},")
+				puts line
+				redo
+			end	
+		end
+			
+		users.each { |user|
+			test_var(user)
+		 
+			if user['name'] == name
+				title.merge!({ 'id_user' => user['id'] })
+				break
+			end
+		}
+		
+		collections << title
+
+	}
+	
+	return collections
 end
 
 def test_var(h)
-    h.each_pair {|key, value| puts "#{key} is #{value}" }	
+    h.each_pair {|key, value| print "#{key} is #{value}, " }
+    puts	
 end
 
 def userLookup(text, db)
@@ -49,7 +149,7 @@ def userLookup(text, db)
 
 	text.each { |line|
 		data = line.split(',')
-		names << data[0]
+		names << data.first
 	}
 
 	names.uniq!
@@ -110,13 +210,13 @@ db.query("create database if not exists #{internaldb};")
 	
 	db.query("CREATE TABLE if not exists users (id tinyint(2) unsigned NOT NULL auto_increment, realname char(64), nickname char(32), email char(64), role tinyint(2), available mediumint(6) default 0, PRIMARY KEY (id)) engine=innodb;")
 
-    db.query("CREATE TABLE if not exists collections (id smallint(4) unsigned NOT NULL auto_increment, id_user tinyint(2) unsigned not null, id_boardgame int(11) not null, status enum('checkin', 'ludoteca', 'demo', 'checkout') default 'checkin', document tinyint(2) unsigned default 0, language char(32), PRIMARY KEY (id), FOREIGN KEY (id_user) REFERENCES users(id) on update cascade on delete cascade, FOREIGN KEY (id_boardgame) REFERENCES boardgames(ID) on update cascade on delete cascade) engine=innodb;")
+    db.query("CREATE TABLE if not exists collections (id smallint(4) unsigned NOT NULL auto_increment, id_user tinyint(2) unsigned not null, id_boardgame int(11) not null, status enum('checkin', 'ludoteca', 'demo', 'checkout') default 'checkin', document tinyint(2) unsigned default 0, language char(2), PRIMARY KEY (id), FOREIGN KEY (id_user) REFERENCES users(id) on update cascade on delete cascade, FOREIGN KEY (id_boardgame) REFERENCES boardgames(ID) on update cascade on delete cascade) engine=innodb;")
 	
     db.query("CREATE TABLE if not exists demos (id smallint(4) unsigned not null auto_increment, id_user tinyint(2) unsigned not null, id_collection smallint(4) unsigned not null, start timestamp default 0, end timestamp default 0, PRIMARY KEY (id), INDEX (id_user,id_collection), FOREIGN KEY (id_user) REFERENCES users(id) on update cascade on delete cascade, FOREIGN KEY (id_collection) REFERENCES collections(id) on update cascade on delete cascade) engine=innodb;")
 
 end
 
-import = ARGV[0]
+import = ARGV.first
 config = ParseConfig.new("gt.conf")
 
 
@@ -128,6 +228,7 @@ rescue Mysql::Error
     exit
 end
 
+#options = parseOptions
 create_tables(dbh, config['internaldb'])
 
 list = File.new(import, 'r')
@@ -135,9 +236,12 @@ text = list.readlines
 
 users = userLookup(text, dbh)
 #users.each { |user| test_var(user); puts }
+puts
+games = gameLookup(text, users)
+createCollections(games, dbh)
+puts
 
-createCollections(text, dbh, users)
-
+#select boardgames.name,boardgames.age,users.realname,users.nickname from collections inner join users on collections.id_user = users.id inner join boardgames on collections.id_boardgame = boardgames.id;
 
 
 
